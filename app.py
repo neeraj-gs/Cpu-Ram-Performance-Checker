523d6a4fbd85076401c3c6482d36d6e641659b3b
neeraj-gs <gsneeraj05@gmail.com>
2023-08-28 09:44:21 +0530

    Added A home route / and created varaibles that extract cpu and mem percent from the server

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import psuitl
 from flask import Flask
+
+app = Flask(__name__) #A flask App created
+
+@app.route('/') #Whenever a user comes in the home path is run 
+
+def index(): #Run this when useer comes at / path
+    cpu_percent = psuitl.cpu_percent #Holds value of CPU Usage
+    mem_percent = psuitl.virtual_memory().percent  #Get the Memory Usage
+    
