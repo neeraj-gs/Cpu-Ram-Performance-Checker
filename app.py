976c43b1cd074b3a3a47c46c42bd19b85bb35dd3
neeraj-gs <gsneeraj05@gmail.com>
2023-08-28 09:48:48 +0530

    Created a Fucntion that gives message if our CPU and Mem is >80%

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ app = Flask(__name__) #A flask App created
 def index(): #Run this when useer comes at / path
     cpu_percent = psuitl.cpu_percent #Holds value of CPU Usage
     mem_percent = psuitl.virtual_memory().percent  #Get the Memory Usage
-    
+    Message = None
+
+    if cpu_percent > 80 or mem_percent > 80: #When the cpu or mem > 80% then needs ot scale up 
+        Message = "High CPU or Memory Utilization Detected. Need to Scale Up IMmediately"
+    return f"CPU Utilization:{cpu_percent} and Memory Utilized:{mem_percent}"
+
+if __name__ == '__main__': #To run on our Local Machine
+    app.run(debug=True,host='0.0.0.0')
